84ef6e1f14a7f95e90a644f28496ee03f0d5bc35
Kandula Suvarna sri <suvarnasri710@gmail.com>
2026-06-19 18:53:26 +0530

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -1,6 +1,16 @@
 create database sampleDb;
 use sampleDb;
 create table student(id int primary key, name varchar(50));
-insert into student value(1, 'ram');
-insert into student values(2, 'parvathi'), (3,'shiv'), (4, 'shiv');
+insert into student value(1, 'ram'), (2, 'parvathi'), (3,'shiv'), (4, 'shiv'),(5, 'Krishna'), (6, 'Ravi'), (7, 'Anjali');
 select * from student;
+
+#output
+| id | name     |
+| -- | -------- |
+| 1  | Ram      |
+| 2  | Parvathi |
+| 3  | Shiv     |
+| 4  | Sita     |
+| 5  | krishna  |
+| 6  | ravi     |
+| 7  | Anjali   |
