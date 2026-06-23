0e5112a7fd0ad0fb4cf537d19212d2c4b619e811
Kandula Suvarna sri <suvarnasri710@gmail.com>
2026-06-23 09:18:46 +0530

    Create sampleDb and student table with data

--- 6. create.sql
+++ 6. create.sql
@@ -1,7 +1,9 @@
 create database sampleDb;
 use sampleDb;
+
 create table student(id int primary key, name varchar(50));
 insert into student value(1, 'ram'), (2, 'parvathi'), (3,'shiv'), (4, 'shiv'),(5, 'Krishna'), (6, 'Ravi'), (7, 'Anjali');
+
 select * from student;
 
 #output
